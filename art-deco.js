b14525d9d39a6e3f37988c0363abe9965456cf42
hridaewcca <35859521+hridaewcca@users.noreply.github.com>
2018-02-09 22:28:03 -0800

    Create art-deco.js

---
+++ art-deco.js
@@ -0,0 +1,8 @@
+for (var a = 5; a <= 20; a = a+5) {
+  noFill();
+  rect (50-a, 170-a, a+a, a+a);
+  rect (110-a, 170-a, a+a, a+a);
+  rect (170-a, 170-a, a+a, a+a);
+  rect (230-a, 170-a, a+a, a+a);
+  rect (290-a, 170-a, a+a, a+a);
+}
